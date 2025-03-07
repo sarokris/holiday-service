2a8a11d34ae2b8d76cfc0aea7b634b3d4a60f55f
skrishna <saravananand.krishnamani@eonics.nl>
2025-03-07 12:31:48 +0100

    Implementation OpenAPI Documentation

---
+++ src/main/java/com/holidays/config/OpenApiConfig.java
@@ -0,0 +1,13 @@
+package com.holidays.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                description = "OpenAPI documentation for holiday service",
+                title = "Holiday service"
+        )
+)
+public class OpenApiConfig {
+}
--- src/main/java/com/holidays/controller/HolidayController.java
+++ src/main/java/com/holidays/controller/HolidayController.java
@@ -4,6 +4,8 @@ import com.holidays.model.LocalHoliday;
 import com.holidays.model.SimpleHoliday;
 import com.holidays.service.HolidayService;
 import com.holidays.model.SortOrder;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,10 +19,14 @@ import java.util.Map;
 @RestController
 @RequestMapping("holidays")
 @AllArgsConstructor
+@Tag(name="Holiday")
 public class HolidayController {
 
     private final HolidayService holidayService;
 
+    @Operation(
+            summary = "This API returns the last celebrated holidays based on the limit"
+    )
     @GetMapping("/last/{countryCode}")
     public List<SimpleHoliday> getLastHolidays(
             @PathVariable String countryCode,
@@ -30,12 +36,18 @@ public class HolidayController {
         return holidayService.getHolidays(countryCode, limit,sortOrder);
     }
 
+    @Operation(
+            summary = "This API returns a number of public holidays not falling on weekends"
+    )
     @GetMapping("/public/weekdays/{year}")
     public Map<String,Long> getWeekHolidayCount(@PathVariable Integer year,@RequestParam List<String> countryCodes){
         return holidayService.getWorkingDayHolidays(year,countryCodes);
     }
+    @Operation(
+            summary = "This API returns the deduplicated list of dates celebrated in both countries with local names"
+    )
     @GetMapping("/common/{year}")
-    public List<LocalHoliday> getWeekHolidayCount(@PathVariable Integer year, @RequestParam String firstCountryCode, @RequestParam String secondCountryCode){
+    public List<LocalHoliday> getCommonHolidays(@PathVariable Integer year, @RequestParam String firstCountryCode, @RequestParam String secondCountryCode){
         return holidayService.getCommonHolidays(year,firstCountryCode,secondCountryCode);
     }
 }
